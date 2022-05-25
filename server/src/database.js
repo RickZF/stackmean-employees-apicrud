088cfdaa03ba61b85717a6e47862556f04519488
RicardoZ <ricardozg7@gmail.com>
2022-05-25 14:05:44 -0500

    Creando la REST API con Express y mongoDB

--- server/src/database.js
+++ server/src/database.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://localhost/stackmean-employees-crud";
+const URI = "mongodb://localhost:27017/stackmean-employees-crud";
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
 })
-.then((db)=> console.log("Db is connected"))
-.catch((err)=>console.error(err));
+.then(db=> console.log("Db is connected"))
+.catch(err=>console.error(err));
 
 module.exports = mongoose;
--- server/src/routes/employeeRoutes.js
+++ server/src/routes/employeeRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const employee = require("../controllers/employeeController");
 
 router.get("/", employee.getEmployees);
+router.get("/:id", employee.getEmployee);
+router.post("/", employee.createEmployee);
+router.put("/:id", employee.editEmployee);
+router.delete("/:id", employee.deleteEmployee);
 
 module.exports = router;
